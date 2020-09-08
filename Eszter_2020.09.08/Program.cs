d821d6c84f1bdd37ca9e96771bd032859dd9bb68
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-09-08 16:10:34 +0200

    Update Program.cs

--- Eszter_2020.09.08/Program.cs
+++ Eszter_2020.09.08/Program.cs
@@ -10,6 +10,20 @@ namespace Eszter_2020._09._08
     {
         static void Main(string[] args)
         {
+            //Írj programot, mely beolvas egy pozitív egész számot, és megmondja, hogy tökéletes szám-e! 
+            //(A tökéletes számok azok, melyek       
+            //osztóinak összege egyenlő a szám kétszeresével. Ilyen szám pl. a 6, mert 2*6 = 1 + 2 + 3 + 6.)
+            Console.Write("Kérem adjon meg egy egész számot! ");
+            int szam = int.Parse(Console.ReadLine());
+            int szamlalo = szam+1;
+            for (int i = 2; i <= szam/2; i++)
+            {
+                if (szam % i == 0)
+                {
+                    szamlalo = szamlalo + i;
+                }
+            }
+
         }
     }
 }
