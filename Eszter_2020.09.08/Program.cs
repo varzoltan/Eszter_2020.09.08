768a37043aa5aff7b30fed518ad97a2a30ebb5bf
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-09-15 16:05:23 +0200

    16:05

--- Eszter_2020.09.08/Program.cs
+++ Eszter_2020.09.08/Program.cs
@@ -10,20 +10,40 @@ namespace Eszter_2020._09._08
     {
         static void Main(string[] args)
         {
-            //Írj programot, mely beolvas egy pozitív egész számot, és megmondja, hogy tökéletes szám-e! 
+            //17. Írj programot, mely beolvas egy pozitív egész számot, és megmondja, hogy tökéletes szám-e! 
             //(A tökéletes számok azok, melyek       
             //osztóinak összege egyenlő a szám kétszeresével. Ilyen szám pl. a 6, mert 2*6 = 1 + 2 + 3 + 6.)
             Console.Write("Kérem adjon meg egy egész számot! ");
             int szam = int.Parse(Console.ReadLine());
-            int szamlalo = szam+1;
-            for (int i = 2; i <= szam/2; i++)
+            int szamlalo = 0;
+            for (int i = 1; i <= szam; i++)
             {
                 if (szam % i == 0)
                 {
                     szamlalo = szamlalo + i;
                 }
             }
+            if (szamlalo == szam*2)
+            {
+                Console.WriteLine($"A felhasználó által megadott szám {szam} tökéletes szám!");
+            }
+            else
+            {
+                Console.WriteLine($"A felhasználó által megadott szám {szam} NEM tökéletes szám!");
+            }
 
+            //18.Olvassunk be egész számokat egy tömbbe(a darabszámot is kérje be a program),
+            //majd írjuk ki a számokat nagyság szerint növekvő sorrendben. (rendezés)
+            Console.Write("Kérem adja meg a tömb elemeinek a számát!");
+            int elemszam = int.Parse(Console.ReadLine());
+            int[] tomb = new int[elemszam];
+            Random veletlen = new Random();
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = veletlen.Next(1,101);
+                Console.Write($"{tomb[i]} ");
+            }
+            Console.ReadKey();
         }
     }
 }
